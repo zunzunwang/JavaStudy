b7721176ad17cce34ae4cf58523b1303f0f9f6f8
zunzunwang <zunzunwang@gmail.com>
2024-03-09 12:42:13 +0100

    test cherry pick

---
+++ java-study-zzw/src/main/java/cherryPick/TestCherryPick2.java
@@ -0,0 +1,9 @@
+package cherryPick;
+
+public class TestCherryPick2 {
+    public static void main(String[] args) {
+        TestCherryPick.sayHello();
+        TestCherryPick.sayBye();
+        TestCherryPick.sayHello();
+    }
+}
